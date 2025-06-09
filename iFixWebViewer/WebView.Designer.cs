416118d31af800f076a23e5736a19d898debcb43
porteetee <por123ja@gmail.com>
2025-06-09 09:32:57 +0700

    ver1

---
+++ iFixWebViewer/WebView.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace iFixWebViewer
+{
+    partial class WebView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(800, 450);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // WebView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.webView21);
+            this.Name = "WebView";
+            this.Size = new System.Drawing.Size(800, 450);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
---
+++ iFixWebViewer/WebView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace iFixWebViewer
+{
+    [ComVisible(true)]
+    [ProgId("iFIXWebView")]
+    [Guid("4B66E666-33D6-4819-8BD6-D87885D3FE9C")]
+
+    public partial class WebView: UserControl
+    {
+
+        public string _url = "";
+        public WebView()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+        private void InitializeWebView()
+        {
+            webView21.Dock = DockStyle.Fill;
+            if (!string.IsNullOrEmpty(_url))
+            {
+                webView21.Source = new Uri(_url);
+            }
+        }
+
+        [ComVisible(true)]
+        public string URL
+        {
+            get
+            {
+                return _url;
+            }
+            set
+            {
+                _url = value;
+                webView21.Source = new Uri(_url);
+            }
+        }
+
+    }
+}
